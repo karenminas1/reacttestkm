f7da8d7bfb46eb19feb5620e3023a74996c75001
Karen Minasyan <karen_minasyan@epam.com>
2019-07-11 18:28:19 +0400

    Updates

---
+++ precache-manifest.e758c164c26edcf6eba53799550da12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5810c5959afaeb6d1f8f4e461ae3b91",
+    "url": "/reacttestkm/index.html"
+  },
+  {
+    "revision": "48179a41e3a044eda9bd",
+    "url": "/reacttestkm/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reacttestkm/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "48179a41e3a044eda9bd",
+    "url": "/reacttestkm/static/js/main.173a7476.chunk.js"
+  },
+  {
+    "revision": "cd6035c5688d5d3162f0",
+    "url": "/reacttestkm/static/js/runtime~main.24f5e7da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttestkm/static/media/logo.5d5d9eef.svg"
+  }
+]);
